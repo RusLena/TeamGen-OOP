9b8cf6f5e63873662d8f71707f73179054cbfa68
RusLena <RusLena@users.noreply.github.com>
2024-02-25 21:55:34 +0000

    wrote code to gather info about employees, created style.css to modify cards

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -13,4 +13,111 @@ const render = require("./src/page-template");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Employees list
 const teamMembers = [];
+
+// add a manager to the team
+function init() {
+    addManager();
+}
+// questions about managers
+function addManager() {
+    inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the manager's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the manager's ID?"
+      },
+
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email?"
+      },
+
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's office number?"
+      }
+
+]).then(answers => {
+    //create a new Manager object with answers
+    const manager = new Manager(answers.name, answers, answers.email, answers.id, answers.officeNumber);
+    // add the new manager to the team
+    teamMembers.push(manager);
+    //continue to adding an engineer
+    addEngineer();
+});
+}
+
+function addEngineer() {
+inquirer.prompt([
+{
+  type: 'input',
+  name: 'name',
+  message: "What is the engineer's name?"
+},
+{
+  type: 'input',
+  name: 'id',
+  message: "What is the engineer's ID?"
+},
+{
+  type: 'input',
+  name: 'email',
+  message: "What is the engineer's email?"
+},
+{
+  type: 'input',
+  name: 'github',
+  message: "What is the engineer's GitHub username?"
+}
+
+]).then(answers => {
+  const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+  teamMembers.push(engineer);
+  addIntern();
+});
+}
+function addIntern() {
+  inquirer.prompt([
+    {
+      type: 'input',
+        name: 'name',
+        message: "What is the intern's name?"
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the intern's ID?"
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the intern's email?"
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: "Which school does the intern attend?"
+    }
+  ]).then(answers => {const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    teamMembers.push(intern);
+    buildTeam();
+  });
+}
+function buildTeam() {
+  // Building the team
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+}
+
+init();
